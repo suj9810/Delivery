69439c27a6cf4b5ddaa63136e9921152bc5dbb06
chohyeokjun <chj3731@naver.com>
2025-05-16 22:56:42 +0900

    feat: QueryDsl로 page 처리된 가게목록 조회

--- src/main/java/com/example/delivery/domain/store/repository/StoreRepositoryCustom.java
+++ src/main/java/com/example/delivery/domain/store/repository/StoreRepositoryCustom.java
@@ -1,5 +1,10 @@
 package com.example.delivery.domain.store.repository;
 
 
+import com.example.delivery.domain.store.dto.response.StoreIdAndNameResponseDto;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+
 public interface StoreRepositoryCustom {
+    Page<StoreIdAndNameResponseDto> findStoreIdAndStoreNameByStoreName(Pageable pageable, String storeName);
 }
--- src/main/java/com/example/delivery/domain/store/repository/StoreRepositoryImpl.java
+++ src/main/java/com/example/delivery/domain/store/repository/StoreRepositoryImpl.java
@@ -1,10 +1,49 @@
 package com.example.delivery.domain.store.repository;
 
+import com.example.delivery.domain.store.dto.response.StoreIdAndNameResponseDto;
+import com.example.delivery.domain.store.entity.QStore;
+import com.querydsl.core.Tuple;
+import com.querydsl.core.types.Projections;
+import com.querydsl.core.types.dsl.BooleanExpression;
+import com.querydsl.jpa.impl.JPAQueryFactory;
 import lombok.RequiredArgsConstructor;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageImpl;
+import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Repository;
+
+import java.util.List;
 
 @Repository
 @RequiredArgsConstructor
 public class StoreRepositoryImpl implements StoreRepositoryCustom{
+
+    private final JPAQueryFactory jpaQueryFactory;
+
+    @Override
+    public Page<StoreIdAndNameResponseDto> findStoreIdAndStoreNameByStoreName(Pageable pageable, String storeName) {
+        QStore qStore = QStore.store;
+
+        List<StoreIdAndNameResponseDto> dtoList = jpaQueryFactory
+                .select(Projections.constructor(StoreIdAndNameResponseDto.class, qStore.id, qStore.storeName))
+                .from(qStore)
+                .where(hasStoreName(storeName))
+                .orderBy(qStore.storeName.asc())
+                .offset(pageable.getOffset())
+                .limit(pageable.getPageSize())
+                .fetch();
+
+        Long total = jpaQueryFactory.
+                select(qStore.countDistinct())
+                .from(qStore)
+                .where(hasStoreName(storeName))
+                .fetchOne();
+
+        return new PageImpl<>(dtoList, pageable, total);
+    }
+
+    private BooleanExpression hasStoreName (String storeName) {
+        return storeName != null ? QStore.store.storeName.containsIgnoreCase(storeName) : null;
+    }
 
 }
