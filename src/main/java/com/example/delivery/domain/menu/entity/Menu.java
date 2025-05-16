3d358ad2f51c6830e0ec5c3fc783250ede849e72
suj9810 <suj9810@naver.com>
2025-05-16 16:46:43 +0900

    init: Menu entity, repository, service, controller 초기 설정

---
+++ src/main/java/com/example/delivery/domain/menu/entity/Menu.java
@@ -0,0 +1,44 @@
+package com.example.delivery.domain.menu.entity;
+
+import com.example.delivery.common.entity.BaseTimeEntity;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.FetchType;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+import jakarta.persistence.Table;
+import lombok.AccessLevel;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Table(name = "menus")
+@Getter
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+public class Menu extends BaseTimeEntity {
+
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Long id;
+
+	// @ManyToOne(fetch = FetchType.LAZY)
+	// @JoinColumn(name = "store_id", nullable = false)
+	// private Store storeId;
+
+	@Column(nullable = false, length = 100)
+	private String name;
+
+	@Column(columnDefinition = "TEXT")
+	private String description;
+
+	@Column(nullable = false)
+	private int price;
+}
